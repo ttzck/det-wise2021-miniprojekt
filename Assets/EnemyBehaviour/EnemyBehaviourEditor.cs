b85d909f2f179fcf47c805f5e2d65e12fd5c1e7a
Jan <jnttzck@gmail.com>
2020-11-05 21:17:22 +0100

    added patrol path visualisation

---
+++ Assets/EnemyBehaviour/EnemyBehaviourEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EnemyBehaviour))]
+public class EnemyBehaviourEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        EnemyBehaviour enemyBehaviour = target as EnemyBehaviour;
+
+        if (enemyBehaviour.PatrolPoints.Count() > 1)
+        {
+            var patrolPointPositions = enemyBehaviour.PatrolPoints
+                .Select(i => i.position)
+                .ToList();
+
+            Handles.DrawLine(patrolPointPositions.First(), patrolPointPositions.Last());
+
+            if (patrolPointPositions.Count() > 2)
+            {
+                for (int i = 1; i < patrolPointPositions.Count(); i++)
+                {
+                    Handles.DrawLine(patrolPointPositions[i], patrolPointPositions[i - 1]);
+                }
+            }
+        }
+    }
+}
