ed8b43f9d3e89cf9e96c5f60cddf2335c11ef38f
Jan <jnttzck@gmail.com>
2020-11-05 19:36:37 +0100

    added patrolling

---
+++ Assets/EnemyBehaviour/EnemyBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour, ICharacterMovementInputs
+{
+    public Vector2 MovementDirection { get; set; }
+
+    public Vector2 RotationTarget { get; set; }
+
+    public IEnemyBehaviourState CurrentState { get; set; }
+
+    public List<Transform> PatrolPoints => patrolPoints;
+
+
+    [SerializeField] private List<Transform> patrolPoints = default;
+
+
+    private void Start()
+    {
+        MovementDirection = transform.position;
+        RotationTarget = transform.position;
+
+        if (patrolPoints.Count > 0)
+        {
+            CurrentState = new PatrollingState(this);
+        }
+    }
+
+    private void Update()
+    {
+        CurrentState?.Update();
+    }
+}
---
+++ Assets/EnemyBehaviour/PatrollingState.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrollingState : IEnemyBehaviourState
+{
+    private const float pointReachedThreshold = .1f;
+
+    private int patrolPointIndex = 0;
+
+    private EnemyBehaviour enemyBehaviour;
+
+    private List<Transform> patrolPoints;
+
+    public PatrollingState(EnemyBehaviour enemyBehaviour) 
+    {
+        this.enemyBehaviour = enemyBehaviour;
+        patrolPoints = enemyBehaviour.PatrolPoints;
+    }
+
+    public void Update()
+    {
+        UpdatePatrolPointIndex();
+
+        enemyBehaviour.MovementDirection = DirectionToCurrentPatrolPoint;
+        enemyBehaviour.RotationTarget = CurrentPatrolPoint.position;
+    }
+
+    private void UpdatePatrolPointIndex()
+    {
+        if (DistanceToCurrentPatrolPoint < pointReachedThreshold)
+        {
+            patrolPointIndex++;
+
+            if (patrolPointIndex >= patrolPoints.Count)
+            { patrolPointIndex = 0; }
+        }
+    }
+
+    private Transform CurrentPatrolPoint => patrolPoints[patrolPointIndex];
+
+    private float DistanceToCurrentPatrolPoint => 
+        Vector2.Distance(enemyBehaviour.transform.position, CurrentPatrolPoint.position);
+
+    private Vector2 DirectionToCurrentPatrolPoint =>
+        (CurrentPatrolPoint.position - enemyBehaviour.transform.position).normalized;
+}
