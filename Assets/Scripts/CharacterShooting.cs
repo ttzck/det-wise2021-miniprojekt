be858df03bbdf6db64c3f402e2eeb10a1170769a
Alimu <xawkiya717@gmail.com>
2020-11-08 18:47:08 +0100

    laser

---
+++ Assets/Scripts/CharacterShooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterShooting : MonoBehaviour
+{
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float cooldownDuration;
+    [SerializeField] private GameObject laser;
+
+    private float cooldownTimestamp;
+    private ICharacterShootingInputs inputs;
+
+    private void Start()
+    {
+        inputs = GetComponent<ICharacterShootingInputs>();
+    }
+
+    private void Update()
+    {
+        if (inputs.IsShooting && Time.time > cooldownTimestamp)
+        {
+            Instantiate(projectile, transform.position, transform.rotation);
+            cooldownTimestamp = Time.time + cooldownDuration;
+        }
+
+        if (inputs.IsLaserShooting && Time.time > cooldownTimestamp)
+        {
+            Instantiate(laser, transform.position, transform.rotation);
+            cooldownTimestamp = Time.time + cooldownDuration;
+        }
+    }
+}
---
+++ Assets/Scripts/ICharacterShootingInputs.cs
@@ -0,0 +1,8 @@
+﻿public interface ICharacterShootingInputs
+{
+    /// <summary>
+    /// wether the character should be shooting
+    /// </summary>
+    bool IsShooting { get; }
+    bool IsLaserShooting { get; }
+}
