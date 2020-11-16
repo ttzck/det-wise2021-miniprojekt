1efc42143d649b0ba482e5a264dd692d053f53b1
Jan <jnttzck@gmail.com>
2020-11-16 18:49:26 +0100

    added levelFinishZone

---
+++ Assets/Scripts/LevelFinishZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(ScenePicker))]
+public class LevelFinishZone : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask;
+
+    private ScenePicker scenePicker;
+
+    private void Awake()
+    {
+        scenePicker = GetComponent<ScenePicker>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (layerMask.Contains(collision.gameObject.layer))
+        {
+            SceneManager.LoadScene(scenePicker.ScenePath);
+        }
+    }
+}
