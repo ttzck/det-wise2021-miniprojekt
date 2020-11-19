d3a59d68b000d035c0458e99ea7a5a0e696c4cc9
Jan <jnttzck@gmail.com>
2020-11-19 14:54:24 +0100

    more particles, tutorial, fonts

---
+++ Assets/TrailScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailScript : MonoBehaviour
+{
+    private ParticleSystem particle;
+    private Transform target;
+
+    private void Awake()
+    {
+        particle = GetComponent<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        target = transform.parent;
+        transform.parent = null;
+    }
+
+    private void Update()
+    {
+        if (target == null || !target.gameObject.activeSelf)
+        {
+            particle.Stop();
+        }
+        else
+        {
+            transform.position = target.position;
+        }
+    }
+}
